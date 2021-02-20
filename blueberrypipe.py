85ce7a643114f3226a1352355b70e50fc973e365
John David Chibuk <chibuk.jd@gmail.com>
2021-02-19 20:27:08 -0500

    morgans guidance

---
+++ blueberrypipe.py
@@ -0,0 +1,18 @@
+#blueberry pod pipe to fft
+
+#connect to selected pod
+#stream data into buffer
+#perform fft on window 
+
+#MH notes: 
+#provide output x e.g. 10 bins, some distribution across 10 buckets
+#time for fft - 256 - 512 - 10 - 128, 128 samples
+#3 - 4 freq. bins - qnode into penny lane
+#displacement in penny lane 
+
+#sequential learning 
+#chemistry -
+#CLI interface of some format 
+
+
+
